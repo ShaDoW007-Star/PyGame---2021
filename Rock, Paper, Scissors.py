7301c345abca0edcc977e2ac1dd3cbb638111d38
ShaDoW007-Star <84889749+ShaDoW007-Star@users.noreply.github.com>
2021-06-02 11:37:43 +0530

    Add files via upload

---
+++ Rock, Paper, Scissors.py
@@ -0,0 +1,89 @@
+# ---------------------------------------------Rock, Paper, Scissors---------------------------------------------------
+import random
+
+cp = 0
+up = 0
+
+
+def game():
+    l = int(5)
+    # global l
+    global cp
+    global up
+    # l = 5
+    # cp = 0
+    # up = 0
+    while l > 0:
+        x = ["r", "p", "s"]
+        print("R for Rock")
+        print("P for Paper")
+        print("S for Scissors")
+        u = input("Enter Your Choice : ")
+        c = random.choice(x)
+        if u == "r" and c == "p":
+            cp = cp + 1
+            print("Computer is Win...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "p" and c == "s":
+            cp = cp + 1
+            print("Computer is Win...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "s" and c == "r":
+            cp = cp + 1
+            print("Computer is Win...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "r" and c == "s":
+            up = up + 1
+            print("User is Win...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "s" and c == "p":
+            up = up + 1
+            print("User is Win...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "p" and c == "r":
+            up = up + 1
+            print("User is Win...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "s" and c == "s":
+            print("Tie...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "r" and c == "r":
+            print("Tie...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        elif u == "p" and c == "p":
+            print("Tie...!!!")
+            print(f"Your Point {up} and Computer Point {cp}")
+            l -= 1
+            print(f"{l} Life Are There...")
+        else:
+            print("You Have Wrong Input...!!!")
+
+
+game()
+print(f"User Point {up} And Computer Point {cp} ")
+if up > cp:
+    print("User Is Winner...")
+elif up == cp:
+    print("Match is Tie")
+else:
+    print("Computer Is Winner...")
+
+# p = input("Do Your Want To Play Again[Y/N]?")
+# if p == "y":
+#     game()
